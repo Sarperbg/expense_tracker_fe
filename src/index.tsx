58980e33bd9d6acaab43974d8d618363ad02ad15
sarperbg <sarpergulfirat@gmail.com>
2022-09-20 15:02:50 +0300

    second lesson completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
+import rootReducer from './store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <App />
-);
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />,
+    </Provider>,
+    document.getElementById("root"));
 
